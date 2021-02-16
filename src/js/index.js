565ecb6d3eb99b0d5754b2aae6931006101def3d
Norma Meißner <normabmeissner@gmail.com>
2021-02-16 23:33:55 +0100

    mobile Navigation über JS auf der Startseite eingefügt

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,26 @@ myFunction();
 const result = multiply(2, 5);
 
 console.log(result);
+
+// function showMenu() {
+//   const menu = document.getElementById("nav-mobile-black");
+//   menu.classList.add("show");
+// }
+
+function toggleMenu() {
+  const menu = document.getElementById("nav-mobile-black");
+
+  const isIncluded = menu.classList.contains("show");
+
+  if (isIncluded) {
+    menu.classList.remove("show");
+    // remove show class
+  } else {
+    menu.classList.add("show");
+  }
+  // else add show class
+}
+
+const burgerMenu = document.getElementById("burger_menu");
+console.log(burgerMenu);
+burgerMenu.onclick = toggleMenu;
